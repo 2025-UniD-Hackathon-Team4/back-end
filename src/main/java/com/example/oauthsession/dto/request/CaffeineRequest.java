0406dcc230248c49951af0a741d9ee2caba5110f
sammool <sammool2003@gmail.com>
2025-11-15 20:35:03 +0900

    [Feat] 카페인 등록

--- src/main/java/com/example/oauthsession/dto/request/CaffeineRequest.java
+++ src/main/java/com/example/oauthsession/dto/request/CaffeineRequest.java
@@ -1,7 +1,9 @@
 package com.example.oauthsession.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -13,8 +15,13 @@ public class CaffeineRequest {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class AddCaffeineRequestDto{
-        @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSSX")
-        LocalDateTime dateTime;
+        @Schema(
+                description = "섭취 시간 (yyyy-MM-dd'T'HH:mm:ss 형식)",
+                example = "2025-11-15T11:23:45"
+        )
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+         LocalDateTime dateTime;
          String storeName;
          String size;
          String menuName;
