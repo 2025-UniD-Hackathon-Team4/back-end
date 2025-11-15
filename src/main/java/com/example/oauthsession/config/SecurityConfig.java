a48b3c274f7a8842f0f2c511aecc632cd4ecebf8
sammool <sammool2003@gmail.com>
2025-11-16 01:21:39 +0900

    [Feat] 프론트 리다이렉트

--- src/main/java/com/example/oauthsession/config/SecurityConfig.java
+++ src/main/java/com/example/oauthsession/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.oauthsession.config;
 
 import com.example.oauthsession.oauth2.CustomClientRegistrationRepo;
 import com.example.oauthsession.oauth2.CustomOAuth2AuthorizedClientService;
+import com.example.oauthsession.oauth2.OAuth2LoginSuccessHandler;
 import com.example.oauthsession.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,7 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomClientRegistrationRepo customClientRegistrationRepo;
     private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final JdbcTemplate jdbcTemplate;
 
     @Bean
@@ -33,14 +35,14 @@ public class SecurityConfig {
         http.httpBasic((httpBasic)->httpBasic.disable());
         http.oauth2Login(Customizer.withDefaults());
 
-        http
-                .oauth2Login((oauth2) -> oauth2
-                        .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
-                        .authorizedClientService(customOAuth2AuthorizedClientService.
-                                oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepo.clientRegistrationRepository()))
-
-                        .userInfoEndpoint((userInfoEndpointConfig ->
-                                userInfoEndpointConfig.userService(customOAuth2UserService))));
+        http.oauth2Login(oauth2 -> oauth2
+                .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
+                .authorizedClientService(customOAuth2AuthorizedClientService.
+                        oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepo.clientRegistrationRepository()))
+                .userInfoEndpoint(userInfo -> userInfo
+                        .userService(customOAuth2UserService))
+                .successHandler(oAuth2LoginSuccessHandler)
+        );
 
         http
                 .authorizeHttpRequests((auth)-> auth
---
+++ src/main/java/com/example/oauthsession/oauth2/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.example.oauthsession.oauth2;
+
+import com.example.oauthsession.dto.NaverResposne;
+import com.example.oauthsession.entity.User;
+import com.example.oauthsession.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String registrationId = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+
+        NaverResposne oAuth2Response = new NaverResposne(oAuth2User.getAttributes());
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+
+        User user = userRepository.findByUsername(username);
+
+        // 세션에 User 저장
+        request.getSession().setAttribute("LOGIN_USER", user);
+        String targetUrl = "casuon://oauth2redirect/naver";
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
